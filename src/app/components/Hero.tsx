492213122d6cce96d9d02ec5cef8830ac5d37f32
Essa Zaidi <160016180+EAZaidi@users.noreply.github.com>
2024-11-11 00:24:02 +0500

    update Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
       <div className='hidden lg:block'></div>
       <div className='text-[50px] sm:text-[70px] font-bold leading-tight flex justify-around items-center'>
       <div>
-          <p data-aos="zoom-in">I'm</p>
+          <p data-aos="zoom-in">I&apos;m</p>
           <p data-aos="zoom-in">Essa</p>
           <p data-aos="zoom-in">Abbas Zaidi</p>
           </div>
