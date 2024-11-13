17cb3ddbd39b2899a8b496156fc4d0f5e2b0e324
Essa Zaidi <160016180+EAZaidi@users.noreply.github.com>
2024-11-13 14:52:00 +0500

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,14 @@
+"use client"
 import React from 'react'
 import { LuMenu } from "react-icons/lu";
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
   return (
     <div className='container pt-8'>
         <div className='flex justify-between items-center'>
@@ -18,7 +25,31 @@ const Navbar = () => {
                 <li className='menulink'><a href="#skills">My Skills</a></li>
                 <li className='menulink'><a href="#contact">Contact Me</a></li>
             </ul>
-            <LuMenu className='md:hidden' size={30}/>
+            <div className='navbar-menu-icon' onClick={toggleMenu}>
+            {isMenuOpen ? <LuMenu size={30}/>:<LuMenu size={30}/>}
+            </div>
+
+            {isMenuOpen && (
+        <ul className={`navbar-menu ${isMenuOpen ? 'open' : '' }`}>
+          <li className='menulink'>
+            <a href="#hero" onClick={toggleMenu}>Home</a>
+          </li>
+          <li className='menulink'>
+            <a href="#about" onClick={toggleMenu}>About</a>
+          </li>
+          <li className='menulink'>
+            <a href="#projects" onClick={toggleMenu}>Projects</a>
+          </li>
+          <li className='menulink'>
+            <a href="#skills" onClick={toggleMenu}>Skills</a>
+          </li>
+          <li className='menulink'>
+            <a href="#contact" onClick={toggleMenu}>Contact</a>
+          </li>
+
+        </ul>
+      )}
+    
         </div>
     </div>
   )
